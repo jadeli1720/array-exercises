14c4a74a8e2a3629fa7cd657bdade967ec25f440
Dan Levy <dan@danlevy.net>
2019-05-25 02:16:39 -0600

    added instructions for new challenge

--- index.js
+++ index.js
@@ -1,56 +1,56 @@
 
 /**
- * Instructions:
+ * @instructions
  * `sum(numbers): Number`
  * Given an array of numbers, return a sum total of all the numbers.
  */
 function sum (numbers) {
-
+  // TODO: Add your solution here.
 }
 
 /**
- * Instructions:
+ * @instruction
  * `doubleNumbers(numbers): Array`
  * Given an array of numbers, return an array where each element is double its original value.
  */
 function doubleNumbers (numbers) {
-
+  // TODO: Add your solution here.
 }
 
 /**
- * Instructions:
+ * @instruction
  * `doubleCharacters(chars): Array`
  * Given a string, return an array where each element is double its original value. (e.g. 'ab' -> 'aabb')
  */
 function doubleCharacters (chars) {
-
+  // TODO: Add your solution here.
 }
 
 /**
- * Instructions:
+ * @instruction
  * `backwardsify(array): Array`
  * Reverse or flip the values in the array.
  */
 function backwardsify (array) {
-
+  // TODO: Add your solution here.
 }
 
 /**
- * Instructions:
+ * @instruction
  * `interleave(list1, list2): Array`
  * Should return a combined single array, with individual values alternating between list1 & list2.
  */
 function interleave (list1, list2) {
-
+  // TODO: Add your solution here.
 }
 
 /**
- * Instructions:
+ * @instruction
  * `makeRange(count, fillString): Array`
  * Return array of `count` length where each element is set to `fillString`
  */
 function makeRange (count, fillString) {
-
+  // TODO: Add your solution here.
 }
 
 /**
@@ -61,30 +61,57 @@ function makeRange (count, fillString) {
  * @example
  *
  * const input = ['cat', 'kitty', 'catzilla', 'fluffykins']
+ * countByFirstLetter(input)
+ * // will return:
+ * // {
+ * //  c: 2, // cat + catzilla
+ * //  k: 1, // kitty
+ * //  f: 1  // fluffykins
+ * // }
+ */
+
+function countByFirstLetter () {
+  // TODO: Add your solution here.
+}
+
+/**
+ * @instructions
+ * `groupByFirstLetter(words): Object`
+ * Given an array of words, return an object using *lowercase* letters as keys.
+ * The key value should be an array with only the words sharing a starting character.
+ *
+ * @example
+ *
+ * const input = ['Cat', 'Kitty', 'catzilla', 'fluffykins']
  * const result = groupByFirstLetter(input)
  * // result deep equals:
  * // {
- * //  c: 2, // cat + catzilla
- * //  k: 1,
- * //  f: 1
+ * //  c: ['cat', 'catzilla'],
+ * //  k: ['kitty'],
+ * //  f: ['fluffykins']
  * // }
  *
  */
 
-function groupByFirstLetter() {
-
+function groupByFirstLetter () {
+  // TODO: Add your solution here.
 }
 
-///////// END OF CHALLENGE /////////
-///////// END OF CHALLENGE /////////
-///////// END OF CHALLENGE /////////
+/// ////// END OF CHALLENGE /////////
+/// ////// END OF CHALLENGE /////////
+/// ////// END OF CHALLENGE /////////
 
-// IGNORE: Test/Env Detection Stuff //
 if (typeof exports !== 'undefined') {
-  // Node/Non-browser test env
-  module.exports = { sum, doubleCharacters,
+  // IGNORE: Test/Env Detected
+  // For Node/Non-browser test env
+  module.exports = {
+    sum,
+    doubleCharacters,
     doubleNumbers,
     backwardsify,
-    interleave, makeRange, groupByFirstLetter}
+    interleave,
+    makeRange,
+    countByFirstLetter,
+    groupByFirstLetter
+  }
 }
-
